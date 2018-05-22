1c8b4e5869c2fcd3f08f9b2ebc8a96b9f08d0da5
Philippe Magno <philippemagno@gmail.com>
2018-05-22 16:28:19 -0300

    Project init

---
+++ src/main/kotlin/com/sample/treinamento/TreinamentoApplication.kt
@@ -0,0 +1,11 @@
+package com.sample.treinamento
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TreinamentoApplication
+
+fun main(args: Array<String>) {
+    runApplication<TreinamentoApplication>(*args)
+}
---
+++ src/test/kotlin/com/sample/treinamento/TreinamentoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.sample.treinamento
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TreinamentoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
